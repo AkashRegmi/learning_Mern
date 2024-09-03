a1f7db8f4d91d032e21d18fa401464838cf0c7a3
Akash Regmi <akashregmi82@gmail.com>
2024-09-03 22:19:04 +0545

    UseReducer

---
+++ react-basics/src/CounterReducer.jsx
@@ -0,0 +1,45 @@
+import "./App.css";
+import { useReducer } from "react";
+
+// useState, useEffect( celanup) , useREducer (userQuery), useRef, useCallback, useMemo, useContext
+
+function reducer(state, action) {
+  console.log({ state, action });
+  if (action === "increment") {
+    return state + 1;
+  } else if (action === "decrement") {
+    return state - 1;
+  }
+}
+
+function CounterReducer() {
+  // const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, 0);
+
+  const incrementCountByOne = () => {
+    // setcount(count + 1)
+    dispatch("increment");
+  };
+
+  const decrementCountByOne = () => {
+    // setCount(count - 1);
+    dispatch("decrement");
+  };
+
+  return (
+    <>
+      <button
+        style={{
+          color: "red",
+        }}
+        onClick={incrementCountByOne}
+      >
+        +
+      </button>
+      <p>Count: {state}</p>
+      <button onClick={decrementCountByOne}>-</button>
+    </>
+  );
+}
+
+export default CounterReducer;
